738859741e1409a9275f0d213b968053816163a0
ElishaBartov <elisha.bartov@emerson.com>
2022-07-16 22:48:15 +0300

    fixes for Heroku

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 app.set('view engine', 'ejs');
 
 // Server PORT Define
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Set Public path
 const path = require("path");
